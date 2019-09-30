d3c75d55cc3c22e3e8aaabad8f3c83a397fde32d
theLife10 <jdgarcia9@miners.utep.edu>
2019-09-29 20:21:18 -0600

    /new works and it is connected

--- src/new/index.php
+++ src/new/index.php
@@ -1,7 +1,42 @@
 <?php
-echo "hi";
+
+include ("../play/Board.php");
+
+$response = array();
+
+$STRATEGY = $_GET["strategy"];
+
+if(empty( $STRATEGY ) ) {
+    $response = array(
+        'response' => false,
+        'message' => "Strategy not specified"
+    );
+}
+else{
+    
+if($STRATEGY == "Smart" || $STRATEGY == "Random"){
+    $response = array(
+     'response' => true,
+     'pid' => uniqid()
+  );
+}
+else{
+ $response = array(
+   'response' => false,
+    'message' => "Unknown Strategy"
+  );
+        
+ $gameBoard = new Board($STRATEGY);
+ $gameBoard->emptyBoard();
+        
+}
+echo json_encode($response);
+}
+    
+
+   
 ?>
-//this is a test
+
 
 
 
--- src/play/Board.php
+++ src/play/Board.php
@@ -2,15 +2,29 @@
 Class Board{
     //2d array
     var $board = array(array());
-    function __construct(){
+    function __construct() {
+        
         $this->board=array(array());
         
         for($row=0; $row < 6; $row++){
             for($column=0; $column < 7; $column){
                 $this->board[$row][$column] = 0;
+                
             }
+            
         }
         
+    }
+    
+    public static $EMPTY ="";
+    
+    public static function emptyBoard(){
+        if( empty( self::$EMPTY ) ) {
+            
+            self::$EMPTY = new self();
+            
+            return self::$EMPTY;
+        }
     }
 }
 ?>
