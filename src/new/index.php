8fd9973cafc5e269c93ccbfc57b4e59d286bc7b2
theLife10 <jdgarcia9@miners.utep.edu>
2019-09-29 20:32:49 -0600

    cleaned up extra spaces

--- src/new/index.php
+++ src/new/index.php
@@ -8,37 +8,29 @@ $STRATEGY = $_GET["strategy"];
 
 if(empty( $STRATEGY ) ) {
     $response = array(
-        'response' => false,
-        'message' => "Strategy not specified"
-    );
+            'response' => false,
+            'message' => "Strategy not specified"
+            );
 }
 else{
-    
-if($STRATEGY == "Smart" || $STRATEGY == "Random"){
-    $response = array(
-     'response' => true,
-     'pid' => uniqid()
-  );
-}
-else{
- $response = array(
-   'response' => false,
-    'message' => "Unknown Strategy"
-  );
-        
- $gameBoard = new Board($STRATEGY);
- $gameBoard->emptyBoard();
-        
-}
-echo json_encode($response);
-}
-    
 
-   
+    if($STRATEGY == "Smart" || $STRATEGY == "Random"){
+        $response = array(
+                'response' => true,
+                'pid' => uniqid()
+                );
+    }
+    else{
+        $response = array(
+                'response' => false,
+                'message' => "Unknown Strategy"
+                );
+
+        $gameBoard = new Board($STRATEGY);
+        $gameBoard->emptyBoard();
+
+    }
+    echo json_encode($response);
+}
+
 ?>
-
-
-
-
-
-
