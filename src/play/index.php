a0a8ce7b743d1fcae3aec3efb90f8d29603a3aba
theLife10 <jdgarcia9@miners.utep.edu>
2019-10-02 11:15:42 -0600

    made skeleton for play

--- src/play/index.php
+++ src/play/index.php
@@ -1,5 +1,47 @@
 <?php
-    
+
+$move = $_GET['move'];
+
+if( !array_key_exists("pid", $_GET) ){
+    $response = array(
+        'response' => false,
+        'reason' => "PID not specified"
+    );
+    echo json_encode($response);
+}
+/*
+ * comment this if statement to make it to the else statement
+ */
+else if(!file_exists("../saved/".$_GET['pid'].".txt")){
+    $response = array(
+        'response' => false,
+        'reason' => "Cannot find PID"
+    );
+    echo json_encode($response);
+}
+else if(!array_key_exists("move", $_GET)){
+    $response = array(
+        'response' => false,
+        'reason' => "Move not specified"
+    );
+    echo json_encode($response);
+}
+else{
+    $response = array(
+        'response' => true,
+        'move' => array(
+            'slot' => $move,
+            'isWin' => false,
+            'isDraw' => false,
+            'row' => array()
+        )
+        
+    );
+    echo json_encode($response);
+}
+
+
+
 ?>
 
 
