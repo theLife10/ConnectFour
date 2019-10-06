1b68e65d253fe17a944025a4e05b773d9caba514
theLife10 <jdgarcia9@miners.utep.edu>
2019-10-05 19:18:40 -0600

    changed the echo so it can display a chip

--- src/play/Board.php
+++ src/play/Board.php
@@ -3,40 +3,66 @@ Class Board{
     //2d array
     public $gameBoard=array();
     
+    
     function __construct(){
-        for($row=0; $row < 6; $row++){
-            for($column=0; $column < 7; $column++){
-                $this->gameBoard[$row][$column]="#";
+       
+        for($row=0; $row < 7; $row++){
+            for($column=0; $column < 6; $column++){
+                $this->gameBoard[$row][$column]=0;
             }
         }
+       
+       
     }
-    function printBoard(){
-        for ($i = 0; $i < 6; $i++) {
-            for ($j = 0; $j < 7; $j++) {
-                echo $this->gameBoard[$i][$j] . " ";
+    
+    function printBoard($grid){
+        for ($i = 0; $i < 7; $i++) {
+            for ($j = 0; $j < 6; $j++) {
+                echo $grid[$i][$j] . " ";
             }
             echo "<br>";
         }
     }
     
-    public static $EMPTY ="";
     
-    public static function emptyBoard(){
-        if( empty( self::$EMPTY ) ) {
-            
-            self::$EMPTY = new self();
-            
-            return self::$EMPTY;
-        }
-    }
+    function writeInformation($pid){
+        $data = json_decode(file_get_contents("../writable/$pid.txt"),true);
+        $strategy = $data["strategy"];
+        $info = array(
+            "pid" => $pid,
+            "strategy" => $strategy,
+            "grid" => $this->gameBoard,
+            "isWin" => ""          
+        );
+        
+        $file_path = "../writable/" . $pid . '.txt';
+       
+        file_put_contents($file_path, json_encode($info));
+       
+    } 
     
-    public function putToken(){
-        for($i = 6; $i >=0; $i--){
-            if($this->gameBoard != "#"){
-                
-            }
-        }
-    }
+     function dropInSlot($slot, $player) {
+         for($i = 5; $i >= 0; $i--){
+             if($this->gameBoard[$slot][$i] == 0){
+                 $this->gameBoard[$slot][$i] =$player;
+                // exit;
+                break;
+             }
+         }
+         
+     }
+     
+     function refreshBoard($pid){
+         $saved = json_decode(file_get_contents("../writable/" . "$pid.txt"), true);
+         // New instance to restore game
+         $boardd = new self();
+         
+         // Build Board from saved data
+         $boardd->gameBoard = $saved["grid"];
+         
+         return $boardd;
+     }
+ 
    
 }
 
--- src/play/index.php
+++ src/play/index.php
@@ -1,6 +1,11 @@
 <?php
+require_once '../play/Board.php'; 
+require_once '../play/Random.php';
 
 $move = $_GET['move'];
+$pid = $_GET['pid'];
+$currentBoard = new Board();
+$info = json_decode(file_get_contents("../writable/$pid.txt"),true);
 
 if( !array_key_exists("pid", $_GET) ){
     $response = array(
@@ -9,7 +14,7 @@ if( !array_key_exists("pid", $_GET) ){
     );
     echo json_encode($response);
 }
-else if(!file_exists("../saved/".$_GET['pid'].".txt")){
+else if(!file_exists("../writable/".$_GET['pid'].".txt")){
     $response = array(
         'response' => false,
         'reason' => "Unknown pid"
@@ -24,32 +29,60 @@ else if(!array_key_exists("move", $_GET)){
     echo json_encode($response);
 }
 else if($move < 0 || $move >= 7){
-    $response = array(
-        'response' => false,
-        'reason' => "Invalid slot, $move"
-    );
-    echo json_encode($response);
-}
+     $response = array(
+         'response' => false,
+         'reason' => "Invalid slot, $move"
+     );
+     echo json_encode($response);
+ }
 else{
-    $response = array(
-        'response' => true,
-        'ack_move' => array(
-            'slot' => $move,
-            'isWin' => false,
-            'isDraw' => false,
-            'row' => array(),
-            'move' => array(
-                'slot' => $move,
-                'isWin' => false,
-                'isDraw' => false,
-                'row' => array()
-            )
-        )
-    );
+    
+    if(strcasecmp($info["strategy"], "Random") == 0){
+ //     $board = $info["board"]["gameBoard"];
+      
+      $currentBoard ->refreshBoard($pid);
+      
+      
+     //  $random = new Random($board,$move);
+     
+      $randomToken = rand(0,6);
+      $currentBoard->dropInSlot($move, 1);
+      $currentBoard->dropInSlot($randomToken, 2);
+     
+        
+    //   $randomToken = $random->getPosition();
+        
+        
+        
+            $response = array('response'=> true, 
+                'ack_move'=> array(
+                    'slot'=> $move,
+                    'isWin' => false, 
+                    'isDraw' => false, 
+                    'row' =>[] ),
+                'move' => array(
+                    'slot' => $randomToken, 
+                    'isWin' => false, 
+                    'isDraw' => false, 
+                    'row' => []
+                )
+                
+            );
+            
+        
+                
+            
+          
+        
+        $currentBoard -> writeInformation($pid);
+        
+    }
     echo json_encode($response);
+   
 }
-
-
+    
+    
+    
 
 ?>
 
